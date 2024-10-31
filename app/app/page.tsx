75cb778c9b8ecc56ea269ccbc8b25236e0ff1e0e
Krzysztof Staroń <kisiel3141@gmail.com>
2024-10-31 22:19:11 +0100

    yay

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,6 +1,22 @@
+"use client";
+
+import { useEffect } from "react";
 import DailyJournal from "./DailyJournal";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase/config";
 
 const page = () => {
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, user => {
+      if (!user) {
+        window.location.href = "/auth";
+      }
+    });
+
+    return () => {
+      unsub();
+    };
+  });
   return <DailyJournal />;
 };
 
